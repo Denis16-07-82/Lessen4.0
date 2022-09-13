f3f46bdb9d0c04499604636f631d66e50c2dd634
DenisV <naum2891@mail.ru>
2022-09-13 19:03:19 +0300

    add "Вывод массива на экран. "

---
+++ Task4.3/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 3: Напишите программу, которая задаёт массив 
+// из 8 элементов и выводит их на экран.
+
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+
+// 6, 1, 33 -> [6, 1, 33]
+
+double[] arrayA = new double[8];
+for(int i = 0;i < 8;i++)
+{
+    Console.WriteLine($"Введите {i + 1} ый элемент массива");
+    arrayA[i] = Convert.ToDouble(Console.ReadLine());
+}
+System.Console.Write("полученный массив : [");
+for(int i = 0;i < 7;i++)
+{
+    System.Console.Write($"{arrayA[i]}, ");
+}
+System.Console.Write($"{arrayA[7]} ]");
+System.Console.WriteLine();
