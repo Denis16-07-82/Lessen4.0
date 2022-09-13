e5091a097441432cd0ea78da959ea0a77060fd14
DenisV <naum2891@mail.ru>
2022-09-13 18:27:53 +0300

    add "Сумма цифр числа. "

---
+++ Task4.2/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача 2: Напишите программу, которая принимает на
+//  вход число и выдаёт сумму цифр в числе.
+
+// 452 -> 11
+
+// 82 -> 10
+
+// 9012 -> 12
+ulong Prompt(string messege)
+{
+    System.Console.WriteLine(messege);
+    ulong velue = Convert.ToUInt64(Console.ReadLine());
+    return velue;
+}
+ulong velueA = Prompt("Введит число : ");
+
+ulong sumdigits(ulong velue)
+{
+    ulong summ = 0;
+    while (velue > 0)
+    {
+        summ += velue % 10;
+        velue = (velue) / 10;
+    }
+    return summ;
+}
+ulong Summ = sumdigits(velueA);
+System.Console.WriteLine($"Сумма цифр числа {velueA} -> {Summ}");
