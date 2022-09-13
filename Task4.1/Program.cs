5cb22c5be9e079f3508ce79680d7476fdd1fb60c
DenisV <naum2891@mail.ru>
2022-09-13 17:28:53 +0300

    add "Возведение в степень числа. "

---
+++ Task4.1/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 1: Напишите цикл, который принимает на вход два числа (A и B) и
+//  возводит число A в натуральную степень B.
+
+// 3, 5 -> 243 (3⁵)
+
+// 2, 4 -> 16
+
+ulong Prompt(string messege)
+{
+  System.Console.WriteLine(messege);
+  ulong velue = Convert.ToUInt64(Console.ReadLine());
+  return velue;
+}
+ulong velueA = Prompt("Введите число A : ");
+ulong velueB = Prompt("Введите число B : ");
+ulong velueC = 1;
+for(ulong i = 0;i < velueB;i++)
+{
+    velueC = velueC*velueA;
+}
+System.Console.WriteLine($"{velueA} , {velueB} -> {velueC}");
